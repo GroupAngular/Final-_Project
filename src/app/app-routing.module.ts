d2f0eb0642b2e9ab60a882006e4c6fd5fdb13d92
walaa <whussien86@gmail.com>
2025-01-16 09:52:15 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Pages/home/home.component';
+import { AboutUsComponent } from './Pages/about-us/about-us.component';
+import { ContactUsComponent } from './Pages/contact-us/contact-us.component';
+import { PrivacyComponent } from './Pages/privacy/privacy.component';
+import { FAQComponent } from './Pages/faq/faq.component';
+import { CartComponent } from './Pages/cart/cart.component';
+import { WishlistComponent } from './Pages/wishlist/wishlist.component';
+import { ShopComponent } from './Pages/shop/shop.component';
+import { UserComponent } from './Pages/user/user.component';
+import { TermsComponent } from './Pages/terms/terms.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:"Home",component:HomeComponent},
+  {path:"About",component:AboutUsComponent}
+,  {path:"Contact",component:ContactUsComponent}
+,  {path:"privacy",component:PrivacyComponent}
+,  {path:"FQA",component:FAQComponent}
+,  {path:"cart",component:CartComponent}
+,  {path:"shop",component:ShopComponent}
+,
+ {path:"user",component:UserComponent}
+,  {path:"terms",component:TermsComponent}
+
+,  {path:"wishlist",component:WishlistComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,42 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './Shared/nav-bar/nav-bar.component';
+import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './Pages/home/home.component';
+import { ShopComponent } from './Pages/shop/shop.component';
+import { CartComponent } from './Pages/cart/cart.component';
+import { UserComponent } from './Pages/user/user.component';
+import { AboutUsComponent } from './Pages/about-us/about-us.component';
+import { ContactUsComponent } from './Pages/contact-us/contact-us.component';
+import { FAQComponent } from './Pages/faq/faq.component';
+import { TermsComponent } from './Pages/terms/terms.component';
+import { PrivacyComponent } from './Pages/privacy/privacy.component';
+import { WishlistComponent } from './Pages/wishlist/wishlist.component';
+import { CheckoutComponent } from './Pages/checkout/checkout.component'; // Import FormsModule
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HomeComponent,
+    ShopComponent,
+    CartComponent,
+    UserComponent,
+    AboutUsComponent,
+    ContactUsComponent,
+    FAQComponent,
+    TermsComponent,
+    PrivacyComponent,
+    WishlistComponent,
+    CheckoutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    
+    AppRoutingModule,    
+    FormsModule // Add FormsModule to imports
+
   ],
   providers: [],
   bootstrap: [AppComponent]
