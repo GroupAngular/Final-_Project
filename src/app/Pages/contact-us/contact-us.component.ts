a57bbe79cfb2db4456426532782592287b95d492
jehanusama <jehanusama3@gmail.com>
2025-01-17 19:48:02 +0200

    Add Changes To Contact US Page

--- src/app/Pages/contact-us/contact-us.component.ts
+++ src/app/Pages/contact-us/contact-us.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-us',
@@ -9,4 +10,33 @@ import { Component } from '@angular/core';
 })
 export class ContactUsComponent {
 
+  contactForm: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      mobile: ['', [Validators.pattern(/^[0-9]{10}$/)]],
+      subject: [''],
+      message: ['', Validators.required]
+    });
+  }
+
+  ngOnInit(): void {}
+
+  scrollToForm(event: Event): void {
+    event.preventDefault();
+    const formSection = document.querySelector('#formSection');
+    if (formSection) {
+      formSection.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+
+  onSubmit(): void {
+    if (this.contactForm.valid) {
+      console.log('Form Submitted', this.contactForm.value);
+      // Add your form submission logic here
+    } else {
+      this.contactForm.markAllAsTouched();
+    }
+  }
 }
